9777f44b2d87871bc22f9b3f6dc0915f00473761
prakti shalu <prakti0496@gmail.com>
2017-10-21 00:59:28 +0530

    login functionality added

---
+++ public/js/login.js
@@ -0,0 +1,15 @@
+function login(){
+	location.href='../index.html'
+}
+
+function validate(){
+	// Check browser support
+if (typeof(Storage) !== "undefined") {
+    // Store
+    localStorage.setItem("lastname", "Smith");
+    // Retrieve
+    document.getElementById("result").innerHTML = localStorage.getItem("lastname");
+} else {
+    document.getElementById("result").innerHTML = "Sorry, your browser does not support Web Storage...";
+}
+}
--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,27 @@
 console.log("main js called");
-var login_flag = 0;
+var user = {};
+user.test = {};
+user.test.name = "test";
+user.test.company = "test company";
+user.test.email = "test@gmail.com";
+user.test.username = "test username";
+user.test.password = "test";
 function login(){
-	console.log("login button clicked");
-	var login_flag = 1;
-	location.href='../index.html'
+	var login_email = document.getElementById("login_email").value;
+	var login_password = document.getElementById("login_password").value;
+	if(login_email == "test@gmail.com" && login_password == "test"){
+		location.href='../index.html'
+	}
+}
+
+function validate(){
+	// Check browser support
+if (typeof(Storage) !== "undefined") {
+    // Store
+    localStorage.setItem("lastname", "Smith");
+    // Retrieve
+    document.getElementById("result").innerHTML = localStorage.getItem("lastname");
+} else {
+    document.getElementById("result").innerHTML = "Sorry, your browser does not support Web Storage...";
+}
 }
