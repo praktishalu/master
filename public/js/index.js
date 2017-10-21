192808b79356675a2457304930ad20021eb1f8ac
prakti shalu <prakti0496@gmail.com>
2017-10-21 23:35:45 +0530

    sign up local storage added

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,25 @@
 console.log("index.js file called");
+function init(){
+	checkLogIn();
+	validate();
+}
 var slideIndex = 0;
+function checkLogIn(){
+	// code for checking logged in or logged out
+	if (typeof(Storage) !== "undefined") {
+	    if(localStorage.getItem("loggedin") == "true"){
+	    	document.getElementById("login_button").innerHTML = "sign out";
+	    }
+	    else{
+	    	if(localStorage.getItem("loogedin") == "false"){
+	    		document.getElementById("login_button").innerHTML = "Login";
+	    	}
+	    }
+	}
+	else{
+		alert("your browser does not support web Storage..So login functionality cannot be implemented");
+	}
+}
 function validate(){
 	var x = document.getElementsByClassName("category");
 	console.log(x.length);
--- public/js/main.js
+++ public/js/main.js
@@ -6,22 +6,47 @@ user.test.company = "test company";
 user.test.email = "test@gmail.com";
 user.test.username = "test username";
 user.test.password = "test";
+function validate_signout(){
+	if (typeof(Storage) !== "undefined") {
+	    if(localStorage.getItem("loggedin") == "true"){
+	    	localStorage.setItem("loggedin", "false");
+	    	location.href='../index.html';
+	    }
+	}
+	else{
+		alert("your browser does not support web Storage..So login functionality cannot be implemented");
+	}
+}
+validate_signout();
 function login(){
 	var login_email = document.getElementById("login_email").value;
 	var login_password = document.getElementById("login_password").value;
-	if(login_email == "test@gmail.com" && login_password == "test"){
-		location.href='../index.html'
-	}
+		if(login_email == "test@gmail.com" && login_password == "test"){
+			if (typeof(Storage) !== "undefined") {
+				localStorage.setItem("loggedin", "true");
+			}
+			else{
+				alert("your browser does not support web Storage.. So login functionality cannot be implemented");
+			}
+			location.href='../index.html';
+		}
 }
 
-function validate(){
-	// Check browser support
-if (typeof(Storage) !== "undefined") {
-    // Store
-    localStorage.setItem("lastname", "Smith");
-    // Retrieve
-    document.getElementById("result").innerHTML = localStorage.getItem("lastname");
-} else {
-    document.getElementById("result").innerHTML = "Sorry, your browser does not support Web Storage...";
+function sign_up(){
+	alert("sign up button was clicked");
+	var name = document.getElementById("signup_name").value;
+	var company = document.getElementById("signup_company").value;
+	var email = document.getElementById("signup_email").value;
+	var username = document.getElementById("signup_username").value;
+	var password = document.getElementById("signup_password").value;
+	user[name] = {};
+	user[name].name = name;
+	user[name].company = company;
+	user[name].email = email;
+	user[name].username = username;
+	user[name].password = password;
+	localStorage.setItem("user_details", user);
 }
+function validate_signin_email(){
+	console.log(document.getElementById("login_email").value);
 }
